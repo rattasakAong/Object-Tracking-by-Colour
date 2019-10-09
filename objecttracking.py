fd047fd9e53098d92922bf3d76e90bfd02464d8d
rattasakAong <rattasak.khunrak@gmail.com>
2019-10-09 14:30:06 +0700

    increase trackbar

--- objecttracking.py
+++ objecttracking.py
@@ -1,34 +1,61 @@
 import cv2
 import numpy as np
 
+def nothing(x):
+    pass
+
 cap = cv2.VideoCapture(0)
 kernel = np.ones((3, 3), np.uint8)
 kernel_2 = np.ones((5, 5), np.uint8)
+
+# dis_color_1 = np.zeros((300, 500, 3), np.uint8)
+# dis_color_2 = np.zeros((300, 500, 3), np.uint8)
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
 
 while True:
 
     # Take each frame
     _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
+    # frame = cv2.flip(frame, 1)
 
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     cv2.imshow('hsv', hsv)
 
-    # define range of blue color in HSV
-    # lower_blue = np.array([110, 50, 50])
-    # upper_blue = np.array([130, 255, 255])
 
     # define range of orange color in HSV
-    lower_blue = np.array([0, 100, 100])
-    upper_blue = np.array([20, 255, 255])
+    # lower_orange = np.array([0, 100, 100])
+    # upper_orange = np.array([20, 255, 255])
+
+    # define range of color in HSV
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    l_b = np.array([l_h, l_s, l_v])
+    u_p = np.array([u_h, u_s, u_v])
+
+    # dis_color_1[:] = l_b
+    # dis_color_2[:] = u_p
+    #
+    # cv2.imshow('Tracking', dis_color_1)
+    # cv2.imshow('Tracking', dis_color_2)
 
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, l_b, u_p)
 
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel_2)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_2)
 
     # _, mask = cv2.threshold(mask,0,255,cv2.THRESH_BINARY_INV)
     _, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)
@@ -53,6 +80,8 @@ while True:
             cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
 
     # Bitwise-AND mask and original image
+    # res = cv2.bitwise_and(frame, hsv, mask=mask)
+    # res = cv2.bitwise_and(hsv, frame, mask=mask)
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
     cv2.imshow('frame',frame)
